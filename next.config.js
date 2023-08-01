2465f951fa7bfcd58c178930b688abaecae62468
HuskyHsu <schi.hsu@gmail.com>
2023-08-01 23:28:45 +0800

    feat: 🎸 add resources

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['www.serebii.net'], // 將您要許可的主機名稱加入到這個陣列中
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
