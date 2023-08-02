5fdcbbbba07b12695e60cc9ad00f4a191b61c3c3
Shihchi Hsu <schi.hsu@gmail.com>
2023-08-02 13:01:29 +0800

    feat: 🎸 update pm data

--- scripts/01_csv_to_json.py
+++ scripts/01_csv_to_json.py
@@ -3,6 +3,7 @@ import os
 
 import requests
 
+
 def is_number(s):
     try:
         int(s)
@@ -10,8 +11,9 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def download_images(data, output_folder):
-    image_list_column = 'image_list'
+    image_list_column = "image_list"
 
     for item in data:
         if image_list_column in item:
@@ -21,62 +23,75 @@ def download_images(data, output_folder):
                 image_filepath = os.path.join(output_folder, image_filename)
                 download_image(image_url, image_filepath)
 
+
 def download_image(url, filepath):
     response = requests.get(url)
     if response.status_code == 200:
-        with open(filepath, 'wb') as f:
+        with open(filepath, "wb") as f:
             f.write(response.content)
 
+
 def csv_to_json(csv_data):
-    lines = csv_data.strip().split('\n')
-    header = lines[0].split(',')
-    header = [col.strip().lower().replace(' ', '_') for col in header]
-    excluded_columns = ['image_list', 'image', 'image_shiny']
+    lines = csv_data.strip().split("\n")
+    header = lines[0].split(",")
+    header = [col.strip().lower().replace(" ", "_") for col in header]
+    excluded_columns = ["image_list", "image", "image_shiny"]
 
     data = []
     for line in lines[1:]:
-        values = line.split(',')
+        values = line.split(",")
         obj = {}
 
         for i, col in enumerate(header):
             column_value = values[i].strip()
 
             if col not in excluded_columns:
-                if '_' in col:
-                    prefix, suffix = col.split('_', 1)
+                if "_" in col:
+                    prefix, suffix = col.split("_", 1)
                     if suffix.isdigit():
                         obj[prefix] = obj.get(prefix, [])
                         if column_value:
                             obj[prefix].append(column_value)
                     else:
-                        obj[col] = int(column_value) if is_number(column_value) else column_value
+                        obj[col] = (
+                            int(column_value)
+                            if is_number(column_value)
+                            else column_value
+                        )
                 else:
-                    obj[col] = int(column_value) if is_number(column_value) else column_value
+                    obj[col] = (
+                        int(column_value) if is_number(column_value) else column_value
+                    )
             else:
-                if col == 'image_list':
-                    download_image(column_value, f"../public/image/pmList/{values[0][-3:]}.png")
-                elif col == 'image':
-                    download_image(column_value, f"../public/image/pm/{values[0][-3:]}.png")
-                elif col == 'image_shiny':
-                    download_image(column_value, f"../public/image/pm/{values[0][-3:]}_s.png")
-
+                continue
+                if col == "image_list":
+                    download_image(
+                        column_value, f"../public/image/pmList/{values[0][-3:]}.png"
+                    )
+                elif col == "image":
+                    download_image(
+                        column_value, f"../public/image/pm/{values[0][-3:]}.png"
+                    )
+                elif col == "image_shiny":
+                    download_image(
+                        column_value, f"../public/image/pm/{values[0][-3:]}_s.png"
+                    )
 
         data.append(obj)
 
-    return json.dumps(data)
+    return json.dumps(data, indent=2)
 
 
 if __name__ == "__main__":
-    csv_file_path = 'sleep - PM_list.csv'
+    csv_file_path = "sleep - PM_list.csv"
 
-    with open(csv_file_path, 'r', encoding='utf-8') as file:
+    with open(csv_file_path, "r", encoding="utf-8") as file:
         csv_data = file.read()
         json_data = csv_to_json(csv_data)
         # print(json_data)
 
-    output_folder = '../src/data/'
+    output_folder = "../src/data/"
 
-
-    output_file_path = os.path.join(output_folder, 'pmList.json')
-    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+    output_file_path = os.path.join(output_folder, "pmList.json")
+    with open(output_file_path, "w", encoding="utf-8") as output_file:
         output_file.write(json_data)
