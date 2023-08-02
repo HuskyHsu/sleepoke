87112602de823c73d4df06e230f76f9b01222e1d
Shihchi Hsu <schi.hsu@gmail.com>
2023-08-02 12:56:46 +0800

    feat: 🎸 remove domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['www.serebii.net'], // 將您要許可的主機名稱加入到這個陣列中
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
