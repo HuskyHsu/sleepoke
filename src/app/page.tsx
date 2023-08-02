299f5a93861b4bd1cfbaa93605e73d2af6649724
zoeGuava <zoeguava@gmail.com>
2023-08-02 23:39:48 +0800

    feat: 🎸 add PmItemType

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
 import Image from 'next/image';
 import data from '@/data/pmList.json';
 
+type PmItemType = {
+  pid: string;
+  name: string;
+  type: string;
+  sleep_type: string;
+  specialty: string;
+  berry: string;
+  berry_quantity: number;
+  skill: string;
+  skill_description: string;
+  base_frequency: string;
+  carry_limit: number;
+  friendship_points_needed: number;
+  recruit_experience: number;
+  recruit_shards: number;
+  ingredients: string[];
+  location: string[];
+};
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
