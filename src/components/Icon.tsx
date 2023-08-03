b8d2a533cb3ac2571d6ad447ec236413403273c9
HuskyHsu <schi.hsu@gmail.com>
2023-08-03 21:05:09 +0800

    fix: 🐛 image url

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,7 @@
 import { Pokemon, Berrys, Ingredients } from '@/types';
 
 export function PmImage({ pm }: { pm: Pokemon }) {
-  const imgSrc = (pid: string) => `/image/pm/${pid.slice(-3)}.png`;
+  const imgSrc = (pid: string) => `${process.env.PUBLIC_URL}/image/pm/${pid.slice(-3)}.png`;
   return <img
     src={imgSrc(pm.pid)}
     alt={pm.name}
@@ -9,7 +9,7 @@ export function PmImage({ pm }: { pm: Pokemon }) {
 }
 
 export function PmIcon({ pm }: { pm: Pokemon }) {
-  const imgSrc = (pid: string) => `/image/pmIcon/${pid.slice(-3)}.png`;
+  const imgSrc = (pid: string) => `${process.env.PUBLIC_URL}/image/pmIcon/${pid.slice(-3)}.png`;
   return <img
     src={imgSrc(pm.pid)}
     alt={pm.name}
@@ -17,7 +17,7 @@ export function PmIcon({ pm }: { pm: Pokemon }) {
 }
 
 export function BerryIcon({ name }: { name: string }) {
-  const imgSrc = (fileName: string) => `/image/berries/${fileName}.png`;
+  const imgSrc = (fileName: string) => `${process.env.PUBLIC_URL}/image/berries/${fileName}.png`;
   return (
     <img
       src={imgSrc(Berrys[name as keyof typeof Berrys])}
@@ -27,7 +27,7 @@ export function BerryIcon({ name }: { name: string }) {
 }
 
 export function IngredientIcon({ name }: { name: string }) {
-  const imgSrc = (fileName: string) => `/image/ingredients/${fileName}.png`;
+  const imgSrc = (fileName: string) => `${process.env.PUBLIC_URL}/image/ingredients/${fileName}.png`;
   return (
     <img
       src={imgSrc(Ingredients[name as keyof typeof Ingredients])}
