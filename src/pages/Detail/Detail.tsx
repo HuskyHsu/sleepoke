c78b0d4971bdd896d59282b504ea4c4a81231e35
zoeGuava <zoeguava@gmail.com>
2023-08-05 00:27:56 +0800

    feat: 🎸 add title bar graph style

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -4,6 +4,25 @@ import { Pokemon, SleepTypeBgClass } from '@/types';
 import { Icon } from '@/components';
 import dataList from '@/data/pmList.json'
 
+function TitleSlide(props: {
+    title:string,
+}) {
+    const { title } = props;
+
+    return (
+        <div className='relative w-full bg-green-600  pl-2'>
+            <div className='absolute -left-3 h-full w-2 bg-green-600' />
+            <h3 className='text-white'>{title}</h3>
+            <div className={clsx(
+                'absolute right-0 top-0',
+                'h-0 w-0',
+                'border-solid border-transparent border-b-white',
+                'border-b-[24px] border-l-[8px]',
+            )}/>
+        </div>
+    )
+}
+
 function Moves() {
   const { link = '001' } = useParams();
   const pm = dataList.find((pm: Pokemon) => pm.pid === `#${link.padStart(4, '0')}`) || dataList[0]
@@ -11,19 +30,27 @@ function Moves() {
 
   return (
     <section className='space-y-4'>
-        <div className={clsx(
-            'flex justify-center rounded-2xl p-3',
-            sleepTypeBgClass,
-        )}>
-            <div className="space-y-1">
-                <p className="md:text-lg">正常色</p>
+        <div className='relative flex justify-center'>
+            <div className="z-10 space-y-1 text-center">
                 <Icon.Game.PmFull pm={pm} shiny={false}/>
+                <p className="md:text-lg">正常色</p>
             </div>
-            <div className="space-y-1">
-                <p className="md:text-lg">異色</p>
+            <div className="z-10 space-y-1 text-center">
                 <Icon.Game.PmFull pm={pm} shiny={true}/>
+                <p className="md:text-lg">異色</p>
             </div>
+
+            {/* bg banner */}
+            <div className={clsx(
+                'absolute inset-x-0 bottom-0 z-0 h-3/5',
+                'rounded-2xl',
+                sleepTypeBgClass,
+            )}/>
         </div>
+
+        <h2 className='text-center text-2xl'>{pm.name}</h2>
+
+        <TitleSlide title='基本資訊' />
 
         <dl className="grid grid-cols-2 divide-y divide-gray-200">
             <div className="flex flex-col py-3">
