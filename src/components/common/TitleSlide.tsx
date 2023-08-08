2a46b2ccd613666520cb62ea281a85edf67011b3
zoeGuava <zoeguava@gmail.com>
2023-08-09 01:50:32 +0800

    feat: 🎸 adjust TitleSlide title font-size

--- src/components/common/TitleSlide.tsx
+++ src/components/common/TitleSlide.tsx
@@ -10,12 +10,12 @@ export function TitleSlide({title, color = 'bg-green-600'}: Props) {
         <div className="w-full">
             <div className={clsx('relative ml-4 box-content', color)}>
                 <div className={clsx('absolute -left-4 h-full w-2', color)} />
-                <h3 className='ml-2 text-white'>{title}</h3>
+                <h3 className='ml-2 text-lg text-white'>{title}</h3>
                 <div className={clsx(
                     'absolute right-0 top-0',
                     'h-0 w-0',
                     'border-solid border-transparent border-b-white',
-                    'border-b-[24px] border-l-[12px]',
+                    'border-b-[28px] border-l-[12px]',
                 )}/>
             </div>
         </div>
