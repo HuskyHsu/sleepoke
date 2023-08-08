b15eb3b1f61ca1b5124eaf4f280bf6e0a12dd2c3
HuskyHsu <schi.hsu@gmail.com>
2023-08-08 20:17:41 +0800

    fix: 🐛 TitleSlide margin

--- src/components/common/TitleSlide.tsx
+++ src/components/common/TitleSlide.tsx
@@ -1,20 +1,23 @@
 import clsx from "clsx";
 
+type Props = {
+    title: string,
+    color?: string
+}
 
-export function TitleSlide({title}: {
-    title:string,
-}) {
-
+export function TitleSlide({title, color = 'bg-green-600'}: Props) {
     return (
-        <div className='relative box-content w-full bg-green-600'>
-            <div className='absolute -left-4 h-full w-2 bg-green-600' />
-            <h3 className='ml-2 text-white'>{title}</h3>
-            <div className={clsx(
-                'absolute right-0 top-0',
-                'h-0 w-0',
-                'border-solid border-transparent border-b-white',
-                'border-b-[24px] border-l-[16px]',
-            )}/>
+        <div className="w-full">
+            <div className={clsx('relative ml-4 box-content', color)}>
+                <div className={clsx('absolute -left-4 h-full w-2', color)} />
+                <h3 className='ml-2 text-white'>{title}</h3>
+                <div className={clsx(
+                    'absolute right-0 top-0',
+                    'h-0 w-0',
+                    'border-solid border-transparent border-b-white',
+                    'border-b-[24px] border-l-[12px]',
+                )}/>
+            </div>
         </div>
     )
 }
