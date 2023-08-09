6c76e34b59cdc6af4956494cef8fc6c9dbe4bb82
HuskyHsu <schi.hsu@gmail.com>
2023-08-09 21:24:27 +0800

    feat: 🎸 add logo and favicon

--- src/components/common/icon/index.ts
+++ src/components/common/icon/index.ts
@@ -1,5 +1,6 @@
+import { ReactComponent as Logo } from '@/assets/icon/Logo.svg';
 import { ReactComponent as Search } from '@/assets/icon/Search.svg';
 import { ReactComponent as Filter } from '@/assets/icon/Filter.svg';
 import * as Game from './Game';
 
-export { Game, Search, Filter };
+export { Game, Logo, Search, Filter };
--- src/routers/layouts/MainLayout/MainLayout.tsx
+++ src/routers/layouts/MainLayout/MainLayout.tsx
@@ -1,13 +1,18 @@
 import clsx from 'clsx';
 import { Outlet } from 'react-router-dom';
 
+import { Icon } from '@/components';
+
 function MainLayout() {
   return (
     <main className={clsx(
       'my-4 text-slate-800',
       'px-4 md:px-[10vw] lg:px-[15vw]',
     )}>
-      <h1 className="text-3xl">SleePoke</h1>
+      <div className='flex items-center gap-2'>
+        <Icon.Logo className="h-12 w-12 md:h-20 md:w-20" />
+        <h1 className="text-3xl md:text-5xl">SleePoke</h1>
+      </div>
       <hr />
       <Outlet />
     </main>
