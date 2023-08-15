cc231c0e2af3f1a53adca8b006fbe0eb1330157a
HuskyHsu <schi.hsu@gmail.com>
2023-08-16 00:14:46 +0800

    test: 💍 add Navigation

--- src/routers/layouts/MainLayout/MainLayout.tsx
+++ src/routers/layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,32 @@
 import clsx from 'clsx';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import { Icon } from '@/components';
 
 function MainLayout() {
   return (
-    <main className={clsx('my-4 text-slate-800', 'px-4 md:px-[10vw] lg:px-[15vw]')}>
-      <span className='flex w-full justify-center'>
-        <Icon.Logo className='h-[90px] w-[300px]' />
+    <main className={clsx('my-4 text-slate-800', 'px-4 pb-16 md:px-[10vw] md:pb-0 lg:px-[15vw]')}>
+      <span className='flex w-full items-center justify-center md:justify-between'>
+        <Link to={`/`}>
+          <Icon.Logo className='h-[90px] w-[300px]' />
+        </Link>
+        <div className='hidden gap-4 text-2xl md:flex'>
+          <Link to={`/`}>圖鑑</Link>
+          <Link to={`/meal`}>食譜</Link>
+        </div>
       </span>
       <hr />
       <Outlet />
+      <div className='fixed bottom-0 left-0 z-50 h-16 w-full bg-gray-700 md:hidden'>
+        <div className='mx-auto grid h-full max-w-lg grid-cols-2 justify-around font-medium text-gray-400'>
+          <button type='button' className='inline-flex flex-col items-center justify-center px-5'>
+            <Link to={`/`}>圖鑑</Link>
+          </button>
+          <button type='button' className='inline-flex flex-col items-center justify-center px-5'>
+            <Link to={`/meal`}>食譜</Link>
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
