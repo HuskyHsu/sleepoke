45f67e3ae492c6618f3e982cb364fdaa8c7c58b7
HuskyHsu <schi.hsu@gmail.com>
2023-08-16 21:16:03 +0800

    feat: 🎸 update meals filter

--- src/pages/Meal/Meal.tsx
+++ src/pages/Meal/Meal.tsx
@@ -1,11 +1,55 @@
+import { ChangeEvent, useState } from 'react';
 import clsx from 'clsx';
 
-// import { Icon } from '@/components';
 import { meals, ingredients } from '@/data';
-import { Icon, SubTitleSlide } from '@/components';
+import { Icon } from '@/components';
+import { SleepTypeBgClass } from '@/types';
+import { Buttons } from '../List/components';
+
+export type Filter = {
+  type: string;
+  ingredients: Set<string>;
+};
 
 function Meal() {
-  const types = ['咖哩', '沙拉', '飲料、點心'];
+  const [filter, setFilter] = useState<Filter>({
+    type: '咖哩',
+    ingredients: new Set<string>(),
+  });
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setFilter((prevSearch) => ({
+      ...prevSearch,
+      type: value,
+    }));
+  };
+
+  const handleChickChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+
+    setFilter((prevSearch) => {
+      if (checked) {
+        prevSearch.ingredients.add(name);
+      } else {
+        prevSearch.ingredients.delete(name);
+      }
+
+      return {
+        ...prevSearch,
+        ingredients: prevSearch.ingredients,
+      };
+    });
+  };
+
+  const types = [
+    {
+      name: '咖哩',
+      color: SleepTypeBgClass['淺淺入夢'],
+    },
+    { name: '沙拉', color: SleepTypeBgClass['安然入睡'] },
+    { name: '飲料、點心', color: SleepTypeBgClass['深深入眠'] },
+  ];
 
   const ingredientMap = ingredients.reduce(
     (acc, curr) => {
@@ -17,83 +61,139 @@ function Meal() {
   );
 
   return (
-    <div className='mt-4 flex flex-col gap-y-8'>
-      {types.map((type) => (
-        <div key={type}>
-          <SubTitleSlide title={type} />
-          <ul className={clsx('gap-6', 'grid grid-cols-1', 'md:grid-cols-2', 'mt-4')}>
-            {meals
-              .filter((meals) => meals.type === type)
-              .sort((a, b) => {
-                const aCount = a.ingredients.reduce((acc, { count }) => acc + count, 0);
-                const bCount = b.ingredients.reduce((acc, { count }) => acc + count, 0);
+    <div className='flex flex-col gap-6'>
+      <ul className='grid w-full grid-cols-3 justify-around text-center'>
+        {types.map((type, i) => (
+          <div
+            key={type.name}
+            className={clsx(
+              'flex items-center justify-center gap-x-2 border-y-2 border-slate-300 py-4',
+              i === 0 && 'rounded-l-xl border-l-2',
+              i === types.length - 1 && 'rounded-r-xl border-r-2',
+              type.color,
+            )}
+          >
+            <input
+              id={type.name}
+              type='radio'
+              name='list-radio'
+              value={type.name}
+              checked={type.name === filter.type}
+              className='h-4 w-4 border-gray-300 bg-gray-100'
+              onChange={handleInputChange}
+            />
+            <label htmlFor={type.name} className=''>
+              {type.name}
+            </label>
+          </div>
+        ))}
+      </ul>
+      <div>
+        <Buttons
+          list={ingredients.map((ingredient) => ingredient.name)}
+          Icon={Icon.Game.Ingredient}
+          checkSet={filter.ingredients}
+          handleChange={handleChickChange}
+        />
+      </div>
+      <div className='flex flex-col gap-y-4'>
+        <ul className={clsx('gap-4', 'grid grid-cols-1', 'md:grid-cols-2')}>
+          {meals
+            .filter((meals) => meals.type === filter.type)
+            .sort((a, b) => {
+              if (filter.ingredients.size > 0) {
+                let aHas = a.ingredients.some((ingredient) =>
+                  filter.ingredients.has(ingredient.name),
+                );
 
-                if (aCount !== bCount) {
-                  return aCount - bCount;
+                let bHas = b.ingredients.some((ingredient) =>
+                  filter.ingredients.has(ingredient.name),
+                );
+
+                if (aHas !== bHas) {
+                  return aHas ? -1 : 1;
                 }
 
-                return a.ingredients.length - b.ingredients.length;
-              })
-              .map((meal) => {
-                return (
-                  <li
-                    key={meal.name}
-                    className={clsx(
-                      'relative flex',
-                      'items-center gap-0 rounded-xl',
-                      'outline outline-2 outline-offset-4 outline-amber-300',
-                    )}
-                  >
-                    <div className='relative h-20 w-20 rounded-l-xl bg-amber-100'>
-                      <Icon.Game.Meal name={meal.name} />
-                      <span className='absolute bottom-0 right-2 font-bold'>
-                        {meal.ingredients.reduce((acc, { count }) => acc + count, 0)}
-                      </span>
-                    </div>
-                    <div className='flex flex-col justify-between gap-y-2 px-2'>
-                      <h4 className='font-bold'>{meal.name}</h4>
-                      <div className='flex items-center gap-x-2'>
-                        <ul className='flex w-full items-center gap-6'>
-                          {meal.ingredients.length > 0 ? (
-                            meal.ingredients.map(({ name, count }) => (
-                              <li className='relative flex items-center' key={name}>
-                                <div className={clsx('h-10 w-10 rounded-full bg-amber-100 p-1')}>
-                                  <Icon.Game.Ingredient name={name} />
-                                </div>
-                                <span className='absolute -bottom-1 -right-1 w-4 text-xs font-medium'>
-                                  ×{count}
-                                </span>
-                              </li>
-                            ))
-                          ) : (
-                            <li className='relative flex items-center'>
-                              <div className='h-10 w-10 rounded-full bg-amber-100 p-1'></div>
-                              <span className='absolute -right-1 bottom-0 text-xs font-medium'>
-                                &gt;0
+                aHas = a.ingredients.every((ingredient) => filter.ingredients.has(ingredient.name));
+                bHas = b.ingredients.every((ingredient) => filter.ingredients.has(ingredient.name));
+
+                if (aHas !== bHas) {
+                  return aHas ? -1 : 1;
+                }
+              }
+
+              const aCount = a.ingredients.reduce((acc, { count }) => acc + count, 0);
+              const bCount = b.ingredients.reduce((acc, { count }) => acc + count, 0);
+
+              if (aCount !== bCount) {
+                return aCount - bCount;
+              }
+
+              return a.ingredients.length - b.ingredients.length;
+            })
+            .map((meal) => {
+              return (
+                <li
+                  key={meal.name}
+                  className={clsx(
+                    'relative flex border-2 border-amber-300',
+                    'items-center gap-0 rounded-xl',
+                  )}
+                >
+                  <div className={clsx('relative h-20 w-20 rounded-l-xl', 'bg-amber-100')}>
+                    <Icon.Game.Meal name={meal.name} />
+                    <span className='absolute bottom-0 right-2 font-bold'>
+                      {meal.ingredients.reduce((acc, { count }) => acc + count, 0)}
+                    </span>
+                  </div>
+                  <div className='flex flex-col justify-between gap-y-2 px-2'>
+                    <h4 className='font-bold'>{meal.name}</h4>
+                    <div className='flex items-center gap-x-2'>
+                      <ul className='flex w-full items-center gap-6'>
+                        {meal.ingredients.length > 0 ? (
+                          meal.ingredients.map(({ name, count }) => (
+                            <li className='relative flex items-center' key={name}>
+                              <div
+                                className={clsx(
+                                  'h-10 w-10 rounded-full p-1',
+                                  filter.ingredients.has(name) ? 'bg-amber-400' : 'bg-amber-100',
+                                )}
+                              >
+                                <Icon.Game.Ingredient name={name} />
+                              </div>
+                              <span className='absolute -bottom-1 -right-1 w-4 text-xs font-medium'>
+                                ×{count}
                               </span>
                             </li>
-                          )}
-                        </ul>
-                      </div>
+                          ))
+                        ) : (
+                          <li className='relative flex items-center'>
+                            <div className='h-10 w-10 rounded-full bg-amber-100 p-1'></div>
+                            <span className='absolute -right-1 bottom-0 text-xs font-medium'>
+                              &gt;0
+                            </span>
+                          </li>
+                        )}
+                      </ul>
                     </div>
-                    <span
-                      className={clsx(
-                        'absolute right-0 top-0 w-20 whitespace-nowrap',
-                        'rounded-bl-xl rounded-tr-xl',
-                        'bg-amber-100 px-2 py-1 text-center',
-                      )}
-                    >
-                      {meal.ingredients.reduce(
-                        (acc, { name, count }) => acc + ingredientMap[name] * count,
-                        0,
-                      )}
-                    </span>
-                  </li>
-                );
-              })}
-          </ul>
-        </div>
-      ))}
+                  </div>
+                  <span
+                    className={clsx(
+                      'absolute right-0 top-0 w-20 whitespace-nowrap',
+                      'rounded-bl-xl rounded-tr-xl',
+                      'bg-amber-100 px-2 py-1 text-center',
+                    )}
+                  >
+                    {meal.ingredients.reduce(
+                      (acc, { name, count }) => acc + ingredientMap[name] * count,
+                      0,
+                    )}
+                  </span>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/routers/layouts/MainLayout/MainLayout.tsx
+++ src/routers/layouts/MainLayout/MainLayout.tsx
@@ -5,7 +5,7 @@ import { Icon } from '@/components';
 
 function MainLayout() {
   return (
-    <main className={clsx('my-4 text-slate-800', 'px-4 pb-12 md:px-[10vw] md:pb-0 lg:px-[15vw]')}>
+    <main className={clsx('my-4 text-slate-800', 'px-4 pb-16 md:px-[10vw] md:pb-0 lg:px-[15vw]')}>
       <span className='flex w-full items-center justify-center md:justify-between'>
         <Link to={`/`}>
           <Icon.Logo className='h-[90px] w-[300px]' />
@@ -17,14 +17,14 @@ function MainLayout() {
       </span>
       <hr />
       <Outlet />
-      <div className='fixed bottom-0 left-0 z-50 h-12 w-full bg-custom-brown md:hidden'>
-        <div className='mx-auto grid h-full max-w-lg grid-cols-2 justify-around text-gray-100'>
-          <button type='button' className='inline-flex flex-col items-center justify-center px-5'>
-            <Link to={`/`}>圖鑑</Link>
-          </button>
-          <button type='button' className='inline-flex flex-col items-center justify-center px-5'>
-            <Link to={`/meal`}>食譜</Link>
-          </button>
+      <div className='fixed bottom-0 left-0 z-50 h-16 w-full bg-custom-brown md:hidden'>
+        <div className='mx-auto grid h-full max-w-lg grid-cols-2 items-center justify-around  text-center text-gray-100'>
+          <Link to={`/`} className='border-r leading-[4rem]'>
+            圖鑑
+          </Link>
+          <Link to={`/meal`} className='leading-[4rem]'>
+            食譜
+          </Link>
         </div>
       </div>
     </main>
