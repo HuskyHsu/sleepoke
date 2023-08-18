9805f1e6febc4b453bc38ed56253aad35d14c3af
HuskyHsu <schi.hsu@gmail.com>
2023-08-18 22:39:08 +0800

    style: 💄 updte navigation style

--- src/routers/layouts/MainLayout/MainLayout.tsx
+++ src/routers/layouts/MainLayout/MainLayout.tsx
@@ -5,7 +5,9 @@ import { Icon } from '@/components';
 
 function MainLayout() {
   return (
-    <main className={clsx('my-4 text-slate-800', 'px-4 pb-16 md:px-[10vw] md:pb-0 lg:px-[15vw]')}>
+    <main
+      className={clsx('my-4 text-slate-800', 'px-4 pb-[4.5rem] md:px-[10vw] md:pb-0 lg:px-[15vw]')}
+    >
       <span className='flex w-full items-center justify-center md:justify-between'>
         <Link to={`/`}>
           <Icon.Logo className='h-[90px] w-[300px]' />
@@ -17,9 +19,14 @@ function MainLayout() {
       </span>
       <hr />
       <Outlet />
-      <div className='fixed bottom-0 left-0 z-50 h-16 w-full bg-custom-brown md:hidden'>
-        <div className='mx-auto grid h-full max-w-lg grid-cols-2 items-center justify-around  text-center text-gray-100'>
-          <Link to={`/`} className='border-r leading-[4rem]'>
+      <div className='fixed bottom-0 left-0 z-50 h-16 w-full bg-white shadow-navigation md:hidden'>
+        <div
+          className={clsx(
+            'mx-auto grid h-full max-w-lg grid-cols-2',
+            'items-center justify-around divide-x-2 text-center text-xl',
+          )}
+        >
+          <Link to={`/`} className='leading-[4rem]'>
             圖鑑
           </Link>
           <Link to={`/meal`} className='leading-[4rem]'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,7 @@ module.exports = {
       boxShadow: {
         'list-items': 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
         'list-items--hover': 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
+        navigation: 'rgba(101, 208, 119, 0.6) 0px -4px, rgba(101, 208, 119, 0.4) 0px -8px',
       },
     },
   },
