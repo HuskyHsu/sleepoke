7859b7f0ab0b4b2ba663a05f7a725c77e7e57356
HuskyHsu <schi.hsu@gmail.com>
2023-08-19 11:22:07 +0800

    fix: 🐛 remove ingredient order by point

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,8 @@
 import berries from './berries.json';
-import ingredients_ from './ingredients.json';
+import ingredients from './ingredients.json';
 import skills from './skills.json';
 import pmList from './pmList.json';
 import specialties from './specialties.json';
 import meals from './meals.json';
-
-const ingredients = ingredients_.sort((a, b) => a.point - b.point);
 
 export { pmList, meals, berries, ingredients, skills, specialties };
