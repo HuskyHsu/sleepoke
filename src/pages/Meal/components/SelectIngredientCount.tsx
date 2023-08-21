9863da6a7bad0b809444dd00336ddd78ae77a65e
Shihchi Hsu <schi.hsu@gmail.com>
2023-08-21 13:19:21 +0800

    feat: 🎸 add display ingredient point

--- src/pages/Meal/components/SelectIngredientCount.tsx
+++ src/pages/Meal/components/SelectIngredientCount.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { Icon, SubTitleSlide } from '@/components';
 import { ingredients, meals } from '@/data';
 
@@ -13,6 +14,10 @@ type IngredientsCount = {
   count: number; // -1 is not enough
   list: number[];
 };
+
+const ingredientMap = Object.fromEntries(
+  ingredients.map((ingredient) => [ingredient.name, ingredient.point]),
+);
 
 const getCountList = (filter: Filter): Record<string, IngredientsCount> => {
   return Object.fromEntries(
@@ -61,15 +66,26 @@ export function SelectIngredientCount({ filter, handleCountChange }: Props) {
   return (
     <div className='space-y-4'>
       <SubTitleSlide title='食材數量 (選至少滿足多少個)' />
-      <div className='grid grid-cols-1 md:grid-cols-2'>
+      <div className='grid grid-cols-1 gap-y-2 md:grid-cols-2'>
         {Object.entries(countList)
           .sort((a, b) => b[1].list.length - a[1].list.length)
           .map(([key, val]) => {
             return (
               <div key={key} className='flex items-center gap-3'>
-                <span className='h-12 w-12'>
-                  <Icon.Game.Ingredient name={key} />
-                </span>
+                <div className='flex w-16 flex-col items-center'>
+                  <div className='h-12 w-12'>
+                    <Icon.Game.Ingredient name={key} />
+                  </div>
+                  <span
+                    className={clsx(
+                      '-mt-3 whitespace-nowrap',
+                      'rounded-full border-2 border-amber-300',
+                      'bg-white px-1 text-center text-xs font-medium',
+                    )}
+                  >
+                    {ingredientMap[key]}能量
+                  </span>
+                </div>
                 {val.list.length === 0 && '未用到 / 尚缺其他食材 / 鍋子容量不夠'}
                 {val.list.length > 0 &&
                   val.list.map((count, i) => (
