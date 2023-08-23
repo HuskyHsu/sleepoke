5e6d825bde951fbe1f40a958a7d30d606096bfff
Shihchi Hsu <schi.hsu@gmail.com>
2023-08-23 13:13:12 +0800

    feat: 🎸 update loading

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,9 +1,14 @@
+import { Icon } from '..';
 import './Loading.css';
 
 export function Loading() {
   return (
     <div className='flex h-[50vh] w-full items-center justify-center'>
-      <div className='wobbling' />
+      <div className='wobbling relative'>
+        <span className='absolute bottom-4 left-8'>
+          <Icon.ZZ className='h-24' />
+        </span>
+      </div>
     </div>
   );
 }
--- src/components/common/icon/index.ts
+++ src/components/common/icon/index.ts
@@ -6,7 +6,8 @@ import { ReactComponent as Close } from '@/assets/icon/Close.svg';
 import { ReactComponent as Swipe } from '@/assets/icon/Swipe.svg';
 import { ReactComponent as Before } from '@/assets/icon/Before.svg';
 import { ReactComponent as Next } from '@/assets/icon/Next.svg';
+import { ReactComponent as ZZ } from '@/assets/icon/ZZ.svg';
 
 import * as Game from './Game';
 
-export { Game, Logo, Search, Filter, Category, Close, Swipe, Before, Next };
+export { Game, Logo, Search, Filter, Category, Close, Swipe, Before, Next, ZZ };
